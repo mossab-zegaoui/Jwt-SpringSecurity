65ba62090f575d55e839b7976e74e10c50dfd37c
mossaabze <zegaouimossaab@gmail.com>
2023-02-12 19:21:22 +0100

    initial commit

--- api/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springsecurity.api.repositories;
-
-import com.example.springsecurity.api.entities.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByName(String userName);
-}
